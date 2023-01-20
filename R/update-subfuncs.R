b486d019df1940f5a316b1434689aaf2a348f046
Richard Arnold <arnoldri@gmail.com>
2023-01-20 16:37:25 +0900

    finishing MBT

--- R/update-subfuncs.R
+++ R/update-subfuncs.R
@@ -1,6 +1,6 @@
 # General Fragments for updating
 
-#' eta V1 (IFR/DFR)
+#' eta V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.eta.v1 <- function(state, datlist, fpar, ppar, model,
@@ -49,7 +49,7 @@ update.eta.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' gamma V1 (Gibbs) (IFR/DFR)
+#' gamma V1 (Gibbs) (IFR/DFR/LWB)
 #'
 #' @export
 update.gamma.v1 <- function(state, datlist, fpar, ppar, model,
@@ -67,10 +67,13 @@ update.gamma.v1 <- function(state, datlist, fpar, ppar, model,
 
   llike.old <- state$llike
   gamma.old <- state$gamma
+  
   temp.model.list <- list(model=model, kmax=kmax, 
                           lambda0=state[[nm["eta"]]], ## Note: unscaled
                           thetavec=state[[nm["thetavec"]]],
                           wvec=state[[nm["uvec"]]])   ## Note: unscaled
+  if(model=="LWB") temp.model.list$a <- state[[nm["a"]]]
+  
   cc <- sum(int.lambda.func(datlist$tvec, model.list=temp.model.list))
   g1star <- state[[nm["alpha"]]]+datlist$n0
   g2star <- state[[nm["beta"]]] + cc
@@ -91,7 +94,7 @@ update.gamma.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)  
 }
 
-#' thetavec V1 (IFR/DFR)
+#' thetavec V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.thetavec.v1 <- function(state, datlist, fpar, ppar, model,
@@ -151,7 +154,7 @@ update.thetavec.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' vvec V1 (IFR/DFR)
+#' vvec V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.vvec.v1 <- function(state, datlist, fpar, ppar, model,
@@ -228,7 +231,7 @@ update.vvec.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' alpha V1 (IFR/DFR)
+#' alpha V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.alpha.v1 <- function(state, datlist, fpar, ppar, model,
@@ -282,7 +285,7 @@ update.alpha.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' beta V1 (IFR/DFR)
+#' beta V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.beta.v1 <- function(state, datlist, fpar, ppar, model,
@@ -317,7 +320,7 @@ update.beta.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' phi V1 (IFR/DFR)
+#' phi V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.phi.v1 <- function(state, datlist, fpar, ppar, model,
@@ -353,7 +356,7 @@ update.phi.v1 <- function(state, datlist, fpar, ppar, model,
   return(state)
 }
 
-#' wvec V1 (IFR/DFR)
+#' wvec V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.wvec.v1 <- function(state, datlist, fpar, ppar, model,
@@ -401,7 +404,9 @@ update.wvec.v1 <- function(state, datlist, fpar, ppar, model,
     state[[nm["vvec"]]] <- vvec.new
     state[[nm["uvec"]]] <- uvec.new
     state[[nm["wvec"]]] <- state[[nm["gamma"]]]*state[[nm["uvec"]]]
-    state[[nm["lambda0"]]] <- state[[nm["gamma"]]]*state[[nm["eta"]]]
+    if(model %in% c("IFR","DFR","LWB","SBT","MBT") && !is.na(nm["lambda0"])) {
+       state[[nm["lambda0"]]] <- state[[nm["gamma"]]]*state[[nm["eta"]]]
+    } # no need to do anything in the LCV case where gamma doesn't scale other parameters
     wkmax <- state[[nm["wvec"]]][fpar$kmax]
     
     state$llike <- llikef(state, datlist, fpar, model)
@@ -444,7 +449,7 @@ update.wvec.v1 <- function(state, datlist, fpar, ppar, model,
   return(state) 
 }
     
-#' thetaswap V1 (IFR/DFR)
+#' thetaswap V1 (IFR/DFR/LWB)
 #'
 #' @export
 update.thetaswap.v1 <- function(state, datlist, fpar, ppar, model,
@@ -498,3 +503,235 @@ update.thetaswap.v1 <- function(state, datlist, fpar, ppar, model,
   
   return(state)
 }
+
+#' a V1 (LWB)
+#'
+#' @export
+update.a.v1 <- function(state, datlist, fpar, ppar, model,
+                            nm=c(eta="eta",
+                                 gamma="gamma",
+                                 thetavec="thetavec",
+                                 vvec="vvec",
+                                 alpha="alpha",
+                                 beta="beta",
+                                 phi="phi",
+                                 uvec="uvec",
+                                 wvec="wvec",
+                                 lambda0="lambda0",
+                                 thetaswap="thetaswap")) {
+  
+  state.old <- state
+  a.old <- state.old[[nm["a"]]]
+  a.new <- exp( rnorm(1, log(a.old), ppar$sd.log.a) )
+  state[[nm["a"]]] <- a.new
+  state$llike <- llikef(state, datlist, fpar, model)
+  state$lprior <- lpriorf(state, fpar, model)
+  log.r <- (state$llike - state.old$llike)
+  log.r <- ( log.r + fpar$c1*log(a.new/a.old) 
+             - fpar$c2*(a.new-a.old) )
+  if(ppar$verbose) {
+    cat(sprintf("a: (%g;%g) %g->%g: logr=%g\n",
+                state.old$llike, state$llike,
+                a.old, a.new, log.r))
+  }
+  if(is.nan(log.r) || is.na(log.r) || length(log.r)==0) { ##!!==
+    cat(sprintf("a: %g->%g: logr=%g\n",
+                a.old, a.new, log.r))
+    browser()
+  }
+  if(runif(1)<exp(log.r)) {
+    # accept
+    state$accepted[nm["a"]] <- 1
+    if(ppar$verbose) cat("+")
+  } else {
+    # reject
+    state <- state.old
+    state$accepted[nm["a"]] <- 0
+    if(ppar$verbose) cat("-")
+  }
+  
+  return(state)
+}
+
+#' lambda0 V1 (Gibbs) (LCV)
+#'
+#' @export
+update.lambda0.v1 <- function(state, datlist, fpar, ppar, model,
+                              nm=c(lambda0="lambda0",
+                                   w0="w0",
+                                   gamma="gamma",
+                                   thetavec="thetavec",
+                                   vvec="vvec",
+                                   alpha="alpha",
+                                   beta="beta",
+                                   phi="phi",
+                                   uvec="uvec",
+                                   wvec="wvec",
+                                   thetaswap="thetaswap")) {
+  
+  llike.old <- state$llike
+  temp.model.list <- list(model=model, kmax=kmax, 
+                          lambda0=1, ## Note: unscaled
+                          w0=state[[nm["w0"]]],
+                          thetavec=state[[nm["thetavec"]]],
+                          wvec=state[[nm["wvec"]]]) 
+  cc <- sum(int.lambda.func(datlist$tvec, model.list=temp.model.list))
+  
+  lambda0.old <- state[[nm["lambda0"]]]
+  s1star <- fpar$s1 + datlist$n0
+  s2star <- fpar$s2 + cc
+  lambda0.new <- rgamma(1, s1star, s2star)
+  state[[nm["lambda0"]]] <- lambda0.new
+  state$llike <- llikef(state, datlist, fpar, model)
+  state$lprior <- lpriorf(state, fpar, model)
+  if(ppar$verbose) {
+    cat(sprintf("a: (%g;%g) %g->%g: logr=Gibbs\n",
+                llike.old, state$llike,
+                lambda0.old, lambda0.new))
+  }
+  state$accepted[nm["lambda0"]] <- 1
+
+  return(state)
+}
+
+#' w0 V1 (LCV)
+#'
+#' @export
+update.w0.v1 <- function(state, datlist, fpar, ppar, model,
+                              nm=c(lambda0="lambda0",
+                                   w0="w0",
+                                   gamma="gamma",
+                                   thetavec="thetavec",
+                                   vvec="vvec",
+                                   alpha="alpha",
+                                   beta="beta",
+                                   phi="phi",
+                                   uvec="uvec",
+                                   wvec="wvec",
+                                   thetaswap="thetaswap")) {
+  
+  state.old <- state
+  w0.old <- state.old[[nm["w0"]]]
+  w0.new <- rnorm(1, w0.old, ppar$sd.w0) 
+  state[[nm["w0"]]] <- w0.new
+  state$llike <- llikef(state, datlist, fpar, model)
+  state$lprior <- lpriorf(state, fpar, model)
+  log.r <- (state$llike - state.old$llike)
+  log.r <- ( log.r - 0.5*(w0.new^2-w0.old^2)/(ppar$sd.w0^2) ) 
+  if(ppar$verbose) {
+    cat(sprintf("w0: (%g;%g) %g->%g: logr=%g\n",
+                state.old$llike, state$llike,
+                w0.old, w0.new, log.r))
+  }
+  if(is.nan(log.r) || is.na(log.r) || length(log.r)==0) { ##!!==
+    cat(sprintf("w0: %g->%g: logr=%g\n",
+                w0.old, w0.new, log.r))
+    browser()
+  }
+  if(runif(1)<exp(log.r)) {
+    # accept
+    state$accepted[nm["w0"]] <- 1
+    if(ppar$verbose) cat("+")
+  } else {
+    # reject
+    state <- state.old
+    state$accepted[nm["w0"]] <- 0
+    if(ppar$verbose) cat("-")
+  }
+  
+  return(state)
+}
+
+#' gamma V2 (MH) (LCV,SBT,MBT)
+#'
+#' @export
+update.gamma.v2 <- function(state, datlist, fpar, ppar, model,
+                              nm=c(lambda0="lambda0", # NA in first component of SBT
+                                   w0="w0", #ignored in SBT, MBT
+                                   gamma="gamma",
+                                   thetavec="thetavec",
+                                   vvec="vvec",
+                                   alpha="alpha",
+                                   beta="beta",
+                                   phi="phi",
+                                   uvec="uvec",
+                                   wvec="wvec",
+                                   thetaswap="thetaswap")) {
+  
+  state.old <- state
+  gamma.old <- state.old[[nm["gamma"]]]
+  gamma.new <- exp( rnorm(1, log(gamma.old), ppar$sd.log.gamma) )
+  state[[nm["gamma"]]] <- gamma.new
+  if(model!="LCV" && !is.na(nm[["lambda0"]])) {  
+    # LCV does not scale lambda0 by gamma
+    # lambda0 will be missing for component 1 of SBT
+    state[[nm["lambda0"]]] <- state[[nm["gamma"]]]*state[[nm["eta"]]]
+  }
+  state[[nm["wvec"]]] <- state[[nm["gamma"]]]*state[[nm["uvec"]]]
+  state$llike <- llikef(state, datlist, fpar, model)
+  state$lprior <- lpriorf(state, fpar, model)
+  log.r <- (state$llike - state.old$llike)
+  log.r <- ( log.r + state[[nm["alpha"]]]*log(gamma.new/gamma.old) 
+             - state[[nm["beta"]]]*(gamma.new-gamma.old) )
+  if(ppar$verbose) {
+    cat(sprintf("gamma: (%g;%g) %g->%g: logr=%g\n",
+                state.old$llike, state$llike,
+                gamma.old, gamma.new, log.r))
+  }
+  if(is.nan(log.r) || is.na(log.r) || length(log.r)==0) { ##!!==
+    cat(sprintf("gamma: %g->%g: logr=%g\n",
+                gamma.old, gamma.new, log.r))
+    browser()
+  }
+  if(runif(1)<exp(log.r)) {
+    # accept
+    state$accepted[nm["gamma"]] <- 1
+    if(ppar$verbose) cat("+")
+  } else {
+    # reject
+    state <- state.old
+    state$accepted[nm["gamma"]] <- 0
+    if(ppar$verbose) cat("-")
+  }
+  
+  return(state)
+}
+
+#' pival V1 (MBT)
+#'
+#' @export
+update.pival.v1 <- function(state, datlist, fpar, ppar, model,
+                            nm=c(pival="pival")) {
+  
+  state.old <- state
+  pival.old <- state.old[[nm["pival"]]]
+  pival.new <- expit( rnorm(1, logit(pival.old), ppar$sd.logit.pival) ) 
+  state[[nm["pival"]]] <- pival.new
+  state$llike <- llikef(state, datlist, fpar, model)
+  state$lprior <- lpriorf(state, fpar, model)
+  log.r <- (state$llike - state.old$llike)
+  log.r <- ( log.r + log(pival.new*(1-pival.new)/(pival.old*(1-pival.old))) ) 
+  if(ppar$verbose) {
+    cat(sprintf("pival: (%g;%g) %g->%g: logr=%g\n",
+                state.old$llike, state$llike,
+                pival.old, pival.new, log.r))
+  }
+  if(is.nan(log.r) || is.na(log.r) || length(log.r)==0) { ##!!==
+    cat(sprintf("pival: %g->%g: logr=%g\n",
+                pival.old, pival.new, log.r))
+    browser()
+  }
+  if(runif(1)<exp(log.r)) {
+    # accept
+    state$accepted[nm["pival"]] <- 1
+    if(ppar$verbose) cat("+")
+  } else {
+    # reject
+    state <- state.old
+    state$accepted[nm["pival"]] <- 0
+    if(ppar$verbose) cat("-")
+  }
+  
+  return(state)
+}
+
