6f7bd2e3dc9bb7e5eba43e97ec958ad669129456
Richard Arnold <richard.arnold@msor.vuw.ac.nz>
2023-10-11 14:24:57 +1300

    trap for LCV overflow of integrated hazard

--- R/modelfuncs.R
+++ R/modelfuncs.R
@@ -602,7 +602,14 @@ llikef <- function(state, datlist, fpar, model) {
                                 model.list=c(list(model=model,kmax=fpar$kmax),
                                              state),
                                 use.Cpp=fpar$use.Cpp, epsilon=fpar$epsilon)
-  retval <- sum(log(hazfuncs[datlist$obs,1]) - hazfuncs[,2])
+  if(model=="LCV" && all(!is.nan(hazfuncs[,1])) 
+                  && any(is.nan(hazfuncs[,2]))) {
+    # Catch the case where the LCV integrated hazard function overflows
+    cat("*")
+    retval <- -Inf
+  } else {
+    retval <- sum(log(hazfuncs[datlist$obs,1]) - hazfuncs[,2])
+  }
   return(retval)
 }
 #' Log likelihood of the current state
